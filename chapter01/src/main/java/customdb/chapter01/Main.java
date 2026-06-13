cc5f0a0406aefe77c7772ddda2e6af3e145318af
Yutosaki <sasayu8190@gmail.com>
2026-06-13 22:47:35 +0900

    refactor: apply format

--- chapter01/src/main/java/customdb/chapter01/Main.java
+++ chapter01/src/main/java/customdb/chapter01/Main.java
@@ -1,7 +1,7 @@
 package customdb.chapter01;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, Database Chapter 01!");
-    }
+  public static void main(String[] args) {
+    System.out.println("Hello, Database Chapter 01!");
+  }
 }
