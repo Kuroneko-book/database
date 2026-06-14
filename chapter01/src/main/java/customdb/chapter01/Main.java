04e60cb966e9d11c7f9e431327d615aff42e9112
Yutosaki <sasayu8190@gmail.com>
2026-06-14 09:39:19 +0900

    refactor: more simpley

--- chapter01/src/main/java/customdb/chapter01/Main.java
+++ chapter01/src/main/java/customdb/chapter01/Main.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
+    // 今回は `insert 1 test` のような形でデータを作成することを想定
     Map<Integer, String> db = new HashMap<>();
     Scanner scanner = new Scanner(System.in);
 
@@ -18,14 +19,14 @@ public class Main {
       String[] input = scanner.nextLine().trim().split(" ");
       String command = input[0].toLowerCase();
 
-      if (command.equals("exit")) {
-        System.out.println("Bye!");
-        break;
-      } else if (command.equals("insert")) {
+      if (command.equals("insert")) {
         db.put(Integer.parseInt(input[1]), input[2]);
       } else if (command.equals("select")) {
         db.forEach((id, name) -> System.out.println("(" + id + ", " + name + ")"));
-      } else if (!command.isEmpty()) {
+      } else if (command.equals("exit")) {
+        System.out.println("Bye!");
+        break;
+      } else {
         System.out.println("Unrecognized command: " + command);
       }
     }
