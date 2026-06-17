9beeb60e2f76be220c63b90b39bba2c282251bb6
Yutosaki <sasayu8190@gmail.com>
2026-06-17 21:22:08 +0900

    feat: add insert and select feat

--- chapter01/src/main/java/customdb/chapter01/Main.java
+++ chapter01/src/main/java/customdb/chapter01/Main.java
@@ -5,30 +5,49 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
-  public static void main(String[] args) {
-    // 今回は `insert 1 test` のような形でデータを作成することを想定
-    Map<Integer, String> db = new HashMap<>();
-    Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Map<Integer, String> db = new HashMap<>();
+        Scanner scanner = new Scanner(System.in);
 
-    while (true) {
-      System.out.print("db > ");
-      if (!scanner.hasNextLine()) {
-        break;
-      }
+        while (true) {
+            System.out.print("db > ");
+            if (!scanner.hasNextLine()) {
+                break;
+            }
 
-      String[] input = scanner.nextLine().trim().split(" ");
-      String command = input[0].toLowerCase();
+            String[] input = scanner.nextLine().trim().split(" ");
+            String command = input[0].toLowerCase();
 
-      if (command.equals("insert")) {
-        db.put(Integer.parseInt(input[1]), input[2]);
-      } else if (command.equals("select")) {
-        db.forEach((id, name) -> System.out.println("(" + id + ", " + name + ")"));
-      } else if (command.equals("exit")) {
-        System.out.println("Bye!");
-        break;
-      } else {
-        System.out.println("Unrecognized command: " + command);
-      }
+            if (command.isEmpty()) {
+                continue;
+            } else if (command.equals("insert")) {
+                if (input.length < 3) {
+                    System.out.println("Usage: insert <id> <name>");
+                } else {
+                    int id = Integer.parseInt(input[1]);
+                    if (db.containsKey(id)) {
+                        System.out.println("Error: Duplicate key " + id);
+                    } else {
+                        db.put(id, input[2]);
+                    }
+                }
+            } else if (command.equals("select")) {
+                if (input.length == 1) {
+                    db.forEach((id, name) -> System.out.println("(" + id + ", " + name + ")"));
+                } else {
+                    int id = Integer.parseInt(input[1]);
+                    if (db.containsKey(id)) {
+                        System.out.println("(" + id + ", " + db.get(id) + ")");
+                    } else {
+                        System.out.println("Record not found.");
+                    }
+                }
+            } else if (command.equals("exit")) {
+                System.out.println("Bye!");
+                break;
+            } else {
+                System.out.println("Unrecognized command: " + command);
+            }
+        }
     }
-  }
 }
