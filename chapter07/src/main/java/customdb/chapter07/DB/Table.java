64546ee5cefc66c577d099e6699e3f326c3c232e
ilmango-ChikaP <ilmango.doge@gmail.com>
2026-07-08 19:31:59 +0900

    apply formatter

--- chapter07/src/main/java/customdb/chapter07/DB/Table.java
+++ chapter07/src/main/java/customdb/chapter07/DB/Table.java
@@ -18,7 +18,9 @@ public class Table {
   private final Schema schema;
   private final RandomAccessFile file;
   private final Map<String, Index> indexes = new HashMap<>();
+
   public record RecordId(int pageNo, int slotNo) {}
+
   public record Record(RecordId recordId, Row row) {}
 
   public Table(Schema schema, Path path) throws IOException {
